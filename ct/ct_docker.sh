59471b5731b73c4aa4d1edae38674c1c9a8cc857
OpenDev <OpenSourceDevelop@users.noreply.github.com>
2026-01-19 12:18:04 +0100

    Update ct_docker.sh with improved comments and structure

--- ct/ct_docker.sh
+++ ct/ct_docker.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-# Skript für den Proxmox Host zum Erstellen des Containers
 
-# 1. Lade das Build-Framework von tteck
+# Header Informationen für das tteck-Framework
+APP="Docker-Arcane"
+var_disk="8"
+var_cpu="2"
+var_ram="2048"
+var_os="debian"
+var_version="12"
+
+# 1. Lade das Build-Framework (erstellt den LXC)
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.sh)
 
-# 2. Definiere die Container-Eigenschaften
+# 2. Container-Einstellungen (wird vom Framework aufgerufen)
 function build_container() {
-  export PCT_OSTYPE="debian"
-  export PCT_OSVERSION="12"
-  export PCT_DISK_SIZE="8"
-  export PCT_CPUS="2"
-  export PCT_RAM="2048"
-  # Startet den Dialog (Name, IP, etc.)
-  build_container 
+  build_container
 }
 
-# 3. Variablen für den Installer
+# 3. Variablen für den Installer (was im LXC passieren soll)
 export FUNCTIONS_FILE_PATH="https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/functions.sh"
-# WICHTIG: Link zu deinem existierenden Installations-Skript
 export INSTALL_SCRIPT="https://raw.githubusercontent.com/OpenSourceDevelop/proxmox/main/DockerArcane.sh"
 
 # 4. Starte den tteck-Installer Prozess
